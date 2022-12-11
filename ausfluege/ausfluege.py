a655ebacd871da648ae6d964c6e851e057ddb098
anna <anna.burger@stud.fhgr.ch>
2022-12-11 18:21:27 +0100

    Abfrage fürs Budget erfassen und Ausflüge planen erstellt.

--- ausfluege/ausfluege.py
+++ ausfluege/ausfluege.py
@@ -2,16 +2,45 @@ from flask import Flask
 from datenbank import abspeichern
 from datenbank import einlesen
 from flask import render_template
+from flask import redirect
+from flask import request
 
 app = Flask("Ausflüge")
 
 @app.route('/') #app.route ist URL, die ausgeführt wird
+#Quelle: https://www.geeksforgeeks.org/python-check-if-dictionary-is-empty/, um zu schauen ob Dict leer
 def ausflug():
-    abspeichern(0,0)
-    return einlesen()
+    finanzen = einlesen()
+    if len(finanzen) == 0:
+        return redirect('budget-erfassen')
+    else:
+        return redirect('ausflug-planen')
+
+@app.route('/budget-erfassen', methods=['GET', 'POST'])
+#Quelle: https://vegibit.com/how-to-use-forms-in-python-flask/
+def budget_erfassen():
+    if request.method == 'GET':
+        return render_template('budget.html')
+    else:  # request.method == 'POST'
+        budget = int(request.form['budget'])
+        abspeichern(budget, 0)
+        return redirect('/ausflug-planen')
+@app.route('/ausflug-planen', methods=['GET', 'POST'])
+def ausflug_planen():
+    if request.method == 'GET':
+        finanzen = einlesen()
+        budget_monat = int(finanzen['budget'])
+        ausgegeben_monat = int(finanzen['spent'])
+        uebrig_monat = budget_monat - ausgegeben_monat
+        return render_template('ausflug.html', uebrig=uebrig_monat)
+    else:  # request.method == 'POST'
+        return "Formular muss noch ergänzt werden"
+
+@app.route('/ausflug-buchen/<ausflug>', methods=['GET', 'POST'])
+@def ausflug_buchen()
+    return "Ausflug gebucht"
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5001)
 
-# nächste Steps: 1. Datum einlesen, schauen ob Dict leer .. wenn ja = Budget für den Monat anfragen und spent auf 0 setzen und abspeichern
-# wenn nein = Budget & Spent aus dem Dict nehmen / Budget und Spent als Integer umwandeln
+# nächste Schritte: Ausflüge erfassen im Excel
